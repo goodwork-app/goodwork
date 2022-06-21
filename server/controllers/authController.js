bb7ea89d383252053ed021be7ac715c901f6b2e0
James Edwards <j.olden.edwards@gmail.com>
2022-06-21 15:59:56 -0700

    createUser and createSession (untested)
    Co-authored-by: Loralyn Milcarek <98704514+loralyn-milcarek@users.noreply.github.com>
    Co-authored-by: Robby Tipton <100332566+RobbyTipton@users.noreply.github.com>
    Co-authored-by: Tony Lei <100812599+tonylei00@users.noreply.github.com>
    Co-authored-by: James Edwards <2181514+Fredwards@users.noreply.github.com>

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,10 @@
-// require mongoose
-// require the user model
-// require bCrypt
+// require the models
+const models = require('../models/models');
+
+
+// bcrypt necessities
+const saltRounds = 10; 
+const bcrypt = require('bcrypt');
 
 const authController = {};
 
@@ -10,7 +14,7 @@ username, password, and name will be properties on req.body
 check the user model for the username
   if the username is not available set res.body.available to false
   else
-    set res.locals.available to true
+    set res.locals.createSession to true
     create hashed password for the user with bcrypt
     create a new user with the user model - return the _id
     attach the id as res.locals.userID
@@ -18,6 +22,31 @@ return next
 
 */
 authController.createUser = function (req, res, next) {
+  models.User.find({username: req.body.userename})
+    .then((records) => {
+      if(records.length === 0) {
+        console.log(`Username '${req.body.username} is available. Creating now...`);
+        res.locals.createSession = true;
+        //hash and store the new user's password
+        return bcrypt.hash(req.body.password, saltRounds);
+      } else {
+        console.log(`Username '${req.body.username} is not available.`);
+        res.locals.createSession === false;
+        return next();
+      }
+    })
+    .then((hash) => 
+      models.User.create({
+        username: req.body.username,
+        password: hash,
+      })
+    )
+    .then(() => {
+      return next();
+    })
+    .catch((err) => 
+      console.log('An error occurred while creating a new user account: ', err)
+    );
 
 };
 
@@ -30,12 +59,28 @@ authController.signIn = function (req, res, next) {
 };
 
 /*
-The userid will be available at res.locals.userID
-insert a session document into the sssions model
+if(res.locals.available === false )
+The username will be available at req.body.username
+retrieve the userid from the database via the username
+insert a session document into the sssions model with the _id 
 create a cookie using res.cookie() with the userID
 return next
 
 */
 authController.createSession = function (req, res, next) {
-
+  if(!res.locals.createSession){
+    return next();
+  } else {
+    models.User.find({username: req.body.username})
+      .then((userArray) => {
+        const userID = userArray[0]._id;
+        return models.Session.create({userID});
+      })
+      then(() => {
+        return next()
+      })
+      .catch( (err) => 
+        console.log(`There was an error creating a session for user: ${req.body.username} with userID: ${userID} - `, err)
+      );
+  }
 };
