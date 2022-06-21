0bafbe5ccffc843da1282c4aac5264a8b369ccea
loralyn-milcarek <loralyn.milcarek@gmail.com>
2022-06-21 15:03:30 -0400

    added React Router routes
    Co-authored-by: Loralyn Milcarek <98704514+loralyn-milcarek@users.noreply.github.com>
    Co-authored-by: Robby Tipton <100332566+RobbyTipton@users.noreply.github.com>
    Co-authored-by: Tony Lei <100812599+tonylei00@users.noreply.github.com>
    Co-authored-by: James Edwards <2181514+Fredwards@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-
-import React from "react";
-import { Outlet, Link } from "react-router-dom";
-import 'bootstrap';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+import AddJob from "./routes/addjob.jsx";
+import AddPriorities from "./routes/addpriorities.jsx";
+import AddValues from "./routes/addvalues.jsx";
+import Dashboard from "./routes/dashboard.jsx";
+import Job from "./routes/job.jsx";
+import Priorities from "./routes/priorities.jsx";
+import Profile from "./routes/profile.jsx";
+import Values from "./routes/values.jsx"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import LoginModule from './components/login.jsx';
 
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <div>
-        <h1>
-          this is { name }
-        </h1>
-        <Link to="/profile"> Profile </Link>
-        <button type="button" className="btn btn-primary">
-          do goodwork
-        </button>
-        <Outlet />
-      </div>
-    );
-  }
-}
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginModule />}/>
+      <Route path="addjob" element={<AddJob/>}/>
+      <Route path="addpriorities" element={<AddPriorities/>}/>
+      <Route path="addvalues" element={<AddValues/>}/>
+      <Route path="dashboard" element={<Dashboard/>}/>
+      <Route path="job" element={<Job/>}/>
+      <Route path="priorities" element={<Priorities/>}/>
+      <Route path="profile" element={<Profile/>}/>
+      <Route path="values" element={<Values/>}/>
+    </Routes>
+  </BrowserRouter>
+  );
+};
+
 
 export default App;
