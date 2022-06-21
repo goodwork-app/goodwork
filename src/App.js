62e967c8b3398532d4c1c3f4d77d076f98ef2f50
loralyn-milcarek <loralyn.milcarek@gmail.com>
2022-06-21 17:52:55 -0400

    added React Router routes
    Co-authored-by: Loralyn Milcarek <98704514+loralyn-milcarek@users.noreply.github.com>
    Co-authored-by: Robby Tipton <100332566+RobbyTipton@users.noreply.github.com>
    Co-authored-by: Tony Lei <100812599+tonylei00@users.noreply.github.com>
    Co-authored-by: James Edwards <2181514+Fredwards@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
-import AddJob from "./routes/addjob.jsx";
-import AddPriorities from "./routes/addpriorities.jsx";
-import AddValues from "./routes/addvalues.jsx";
-import Dashboard from "./routes/dashboard.jsx";
-import Job from "./routes/job.jsx";
-import Priorities from "./routes/priorities.jsx";
-import Profile from "./routes/profile.jsx";
-import Values from "./routes/values.jsx"
+import Dashboard from "./components/dashboard.jsx";
+import Profile from "./components/profile.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginModule from './components/login.jsx';
 
@@ -16,14 +10,8 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<LoginModule />}/>
-      <Route path="addjob" element={<AddJob/>}/>
-      <Route path="addpriorities" element={<AddPriorities/>}/>
-      <Route path="addvalues" element={<AddValues/>}/>
       <Route path="dashboard" element={<Dashboard/>}/>
-      <Route path="job" element={<Job/>}/>
-      <Route path="priorities" element={<Priorities/>}/>
       <Route path="profile" element={<Profile/>}/>
-      <Route path="values" element={<Values/>}/>
     </Routes>
   </BrowserRouter>
   );
